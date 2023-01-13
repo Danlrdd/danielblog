272e4bc0335c7aa9953852c77fc046364bd39f95
Daniel Z <dan.zavt@outlook.com>
2023-01-13 16:20:20 -0500

    correción de los margenes laterales de la versión web del main

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,14 @@
 /* se hará un toggle class a la clase .nav agregandole un "active". Al hacerle click al boton*/
-const buttom = document.querySelector(".menu_buttom")
+const button = document.querySelector(".menu_buttom")
 const nav = document.querySelector(".nav")
 
-buttom.addEventListener("click", ()=>{
+// button.addEventListener("click", ()=>{
+//     nav.classList.toggle("active")
+// })
+
+button.addEventListener("click", toggleNavSectionMobile);
+
+function toggleNavSectionMobile(){
+    // const isNavSectionMobileClosed = nav.classList.contains("active");
     nav.classList.toggle("active")
-})
+}
